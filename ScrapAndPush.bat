e5d30d2056e2e16dd138e0420a4a1ba9bdaed9ed
zarahz <zarah.zahreddin@gmail.com>
2019-10-16 22:10:44 +0200

    scraping ... :)

--- ScrapAndPush.bat
+++ ScrapAndPush.bat
@@ -1,8 +1,8 @@
 :loop
 cd E:\Dokumente\workspace\PWP\SimpleWebcrawler
 scrapy runspider .\WebCrawler.py
+
 git add -A
-git add $(git ls-files -o --exclude-standard)
 git commit -m "scraping ... :)"
 git push
 
