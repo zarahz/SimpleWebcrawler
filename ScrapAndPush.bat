2419c424d8f664e8ea2f53403912e8f4876bde36
zarahz <zarah.zahreddin@gmail.com>
2019-10-16 21:04:06 +0200

    scraping ... :)

--- ScrapAndPush.bat
+++ ScrapAndPush.bat
@@ -1,10 +1,9 @@
 :loop
 cd E:\Dokumente\workspace\PWP\SimpleWebcrawler
-echo dir
 scrapy runspider .\WebCrawler.py
 
 git add -A
-git commit -m "made changes"
+git commit -m "scraping ... :)"
 git push
 
 timeout 60
